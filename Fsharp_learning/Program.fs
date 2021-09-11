1c8ff1d610134349a49a8b85b36b1a87bd944905
molochkov.dima@mail.ru <molochkov.dima@mail.ru>
2021-09-11 19:39:48 +0300

    Add wrong record Contact

--- Fsharp_learning/Program.fs
+++ Fsharp_learning/Program.fs
@@ -1,13 +1,27 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open Fsharp_learning
+open Type
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
-
+let from myType =
+    sprintf $"from {myType.FirstName} {myType.Address1}"
+    
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let myType1 =
+        { FirstName = "Dima"
+          MiddleInitial = ""
+          LastName = "Molochkov"
+          EmailAddress = "molochkov.dima@mail.ru"
+          IsEmailVerified = true
+          Address1 = "Russian Federation"
+          Address2 = "Russia"
+          City = "Vladimir"
+          State = "Central District"
+          Zip = "600000"
+          IsAddressValid = false }
+        
+    let myType2 = { myType1 with FirstName = "YMHUK"; LastName = "" }
+        
+    Console.WriteLine($"Hello world {from myType1}")
+    printfn $"Hello world {from myType2}"
+    0
---
+++ Fsharp_learning/Type.fs
@@ -0,0 +1,18 @@
+﻿module Fsharp_learning.Type
+
+type Contact =
+    { FirstName: string
+      MiddleInitial: string
+      LastName: string
+
+      EmailAddress: string
+      //true if ownership of email address is confirmed
+      IsEmailVerified: bool
+
+      Address1: string
+      Address2: string
+      City: string
+      State: string
+      Zip: string
+      //true if validated against address service
+      IsAddressValid: bool }
