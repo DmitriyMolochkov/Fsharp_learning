3208fa3145cd056f5c3c2d660bc5b6161ea24009
molochkov.dima@mail.ru <molochkov.dima@mail.ru>
2021-09-12 16:06:49 +0300

    add discriminated union types

--- Fsharp_learning/Contact.fs
+++ Fsharp_learning/Contact.fs
@@ -5,16 +5,21 @@ type PersonalName =
       MiddleInitial: string option
       LastName: string }
 
+type EmailAddress = EmailAddress of string
+
 type EmailContactInfo =
-    { EmailAddress: string
+    { EmailAddress: EmailAddress
       IsEmailVerified: bool }
+
+type ZipCode = ZipCode of string
+type StateCode = StateCode of string
 
 type PostalAddress =
     { Address1: string
       Address2: string
       City: string
-      State: string
-      Zip: string }
+      State: StateCode
+      Zip: ZipCode }
 
 type PostalContactInfo =
     { Address: PostalAddress
@@ -24,3 +29,20 @@ type Contact =
     { Name: PersonalName
       EmailContactInfo: EmailContactInfo
       PostalContactInfo: PostalContactInfo }
+    
+type CreationResult<'T> = Success of 'T | Error of string
+
+let success e = Success e
+let failure er  = Error er
+
+let CreateEmailAddress (s:string) =
+    if System.Text.RegularExpressions.Regex.IsMatch(s,@"^\S+@\S+\.\S+$")
+    then success (EmailAddress s)
+    else failure "Email address must contain an @ sign"
+
+let CreateStateCode (s: string) =
+    let s' = s.ToUpper()
+    let stateCodes = ["AZ"; "CA"; "NY"; "RU"]
+    if stateCodes |> List.exists ((=) s')
+    then Some (StateCode s')
+    else None
--- Fsharp_learning/Program.fs
+++ Fsharp_learning/Program.fs
@@ -1,24 +1,33 @@
 open System
 open Fsharp_learning
 open Contact
+open Microsoft.FSharp.Reflection
 
 let from contact =
-    $"from %s{contact.Name.FirstName} from %s{contact.PostalContactInfo.Address.State}"
+    let (StateCode code) = contact.PostalContactInfo.Address.State
+    //$"from %s{contact.Name.FirstName} from %s{match contact.PostalContactInfo.Address.State with |StateCode code -> code}"
+    $"from %s{contact.Name.FirstName} from %s{code}"
+    
+
 
 [<EntryPoint>]
 let main argv =
-
-    let contact =
-        { Name = { FirstName = "Dima"; MiddleInitial = None; LastName = "Molochkov" }
-          EmailContactInfo = { EmailAddress = "molochkov.dima@mail.ru"; IsEmailVerified = true }
-          PostalContactInfo =
-              { Address =
-                    { Address1 = "Pushkin street house No. 15"
-                      Address2 = "Apartment No. 33"
-                      City = "Vladimir"
-                      State = "Russian Federation"
-                      Zip = "600001" }
-                IsAddressValid = true } }
-
-    printfn $"Hello world {from contact}"
+    match (CreateEmailAddress "molochkov.dima@mail.ru") with
+    | Success email ->
+        match (CreateStateCode "RU") with
+        | Some stateCode ->
+            let contact =
+                { Name = { FirstName = "Dima"; MiddleInitial = None; LastName = "Molochkov" }
+                  EmailContactInfo = { EmailAddress = email; IsEmailVerified = true }
+                  PostalContactInfo =
+                      { Address =
+                            { Address1 = "Pushkin street house No. 15"
+                              Address2 = "Apartment No. 33"
+                              City = "Vladimir"
+                              State = stateCode
+                              Zip = ZipCode "600001"  }
+                        IsAddressValid = true } }
+            printfn $"Hello world {from contact}"
+        | None -> ()
+    | Error error -> printfn $"Error: %s{error}"
     0
